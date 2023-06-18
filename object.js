940239d7634adbd8cd45ddcdfd002dd725892aca
Md Muzahidul Islam <127314146+Muzahidul01@users.noreply.github.com>
2023-06-18 06:23:30 +0600

    Create object.js

---
+++ object.js
@@ -0,0 +1,31 @@
+// var phone = {
+//     barnd: 'Sumsung',
+//     Colour: Red,
+//     Price: 20000,
+//     Country: Bangladesh
+// }
+
+
+
+
+var review = {
+    Grow: 17,
+    Cloth: 153,
+    Low: 88,
+    New: 1,
+}
+
+
+
+
+// review['Cloth'] = 154;
+// console.log(review);
+// var Find = review['Grow']
+// console.log(Find);
+// var pro = review['Cloth']
+
+// var pro = Object.keys(review)
+// var pro = Object.values(review)
+// console.log(pro);
+// review.Cloth = 150;
+// console.log(review);
